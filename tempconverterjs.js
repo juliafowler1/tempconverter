e9e00a3e51af77e517580cd350fd4fc3f1b12de7
Julia Fowler <juliafowler@live.com>
2019-01-28 19:04:19 -0500

    first commit

---
+++ tempconverterjs.js
@@ -0,0 +1,14 @@
+// function convertTemp(temp, unit) {
+//     console.log(temp, unit);
+//     // console.log((temp - 32) / 1.8);
+//     // console.log((temp * 1.8) + 32);
+//     if (unit === "f") {
+//         console.log((temp * 1.8) + 32);
+//     }
+//     else if (unit === "c") {
+//         console.log((temp - 32) / 1.8);
+//     }
+// }
+// //fahrenheit to celsius subtract 32 divide  by 1.8
+// convertTemp(45, "f");
+
